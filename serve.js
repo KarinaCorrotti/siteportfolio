a8efaf80f3ca66c84d9e67a6878734078f74d587
kakinha <karina.corrotti@gmail.com>
2022-01-26 14:57:08 -0300

    atualizando

--- serve.js
+++ serve.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/site-portifolio'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', (req, res) =>
-    res.sendFile(path.join(__dirname + './dist/site-portifolio/src/index.html')),
+    res.sendFile(path.join(__dirname + '/dist/site-portifolio/src/index.html')),
 );
 
 // Start the app by listening on the default Heroku port
