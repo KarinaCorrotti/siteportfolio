eaff89792332f6d2029a0ac2342808283c76d23e
kakinha <karina.corrotti@gmail.com>
2022-01-26 14:18:52 -0300

    atualizando

--- serve.js
+++ serve.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/site-portifolio'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/site-portifolio/'}),
+    res.sendFile('index.html', {root: 'dist/site-portifolio/src'}),
 );
 
 // Start the app by listening on the default Heroku port
